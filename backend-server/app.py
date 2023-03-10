9e9d59114da14dc6abc46decd7e926837bd75800
rowan <rowan@rowanprice.com>
2023-03-10 11:48:52 +0100

    update to make openai responses easier to read in terminal

--- backend-server/app.py
+++ backend-server/app.py
@@ -10,9 +10,9 @@ openai.api_key = "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"
 
 gpt35turbo = openai.ChatCompletion.create(
   model="gpt-3.5-turbo-0301",
-  messages=[{"role": "user", "content": "Can Whisper AI evaluate the emotional subtance of an audio file before it is transcribed?"}]
+  messages=[{"role": "user", "content": "Think through this step-by-step and give me an answer in the form of a bulleted list that reflect a logical progression in though. Can Whisper AI evaluate the emotional subtance of an audio file before it is transcribed?"}]
 )
 
-print(gpt35turbo["choices"][0]["message"]["content"])
+print(gpt35turbo["choices"][0]["message"]["content"] + "\n\n")
 
 
