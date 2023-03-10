786e082e76319a5c0d89d110258142f96bd850ca
rowan <rowan@rowanprice.com>
2023-03-10 13:00:59 +0100

    komprehend + gpt sample script

--- backend-server/app.py
+++ backend-server/app.py
@@ -5,14 +5,29 @@ import openai
 #from flask import Flask
 
 #app = Flask(__name__)
-
 openai.api_key = "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"
 
 gpt35turbo = openai.ChatCompletion.create(
   model="gpt-3.5-turbo-0301",
-  messages=[{"role": "user", "content": "Think through this step-by-step and give me an answer in the form of a bulleted list that reflect a logical progression in though. Can Whisper AI evaluate the emotional subtance of an audio file before it is transcribed?"}]
+  messages=[{"role": "user", "content": "Think through this step-by-step and give me a passionate answer in the form of a bulleted list that reflect a logical progression in though. Can Whisper AI evaluate the emotional subtance of an audio file before it is transcribed?"}]
 )
 
-print(gpt35turbo["choices"][0]["message"]["content"] + "\n\n")
+gptResponse = gpt35turbo["choices"][0]["message"]["content"]
+
+print( gptResponse + "\n\n")
 
 
+
+import paralleldots
+paralleldots.set_api_key("fUE4KZzTXex4ASebbJKJd4NHbbNWs2gkvNUcXeEUOEc")
+
+# for single sentence
+text=gptResponse
+lang_code="en"
+response=paralleldots.sentiment(text,lang_code)
+print(response)
+
+# for multiple sentence as array
+text=["Come on,lets play together","Team performed well overall.",gptResponse]
+response=paralleldots.batch_sentiment(text)
+print(response)
