af31293c4d0727ff8af57f70591b0d82add94d24
rowan <rowan@rowanprice.com>
2023-03-15 12:11:04 +0100

    simplified backend

--- backend-server/app.py
+++ backend-server/app.py
@@ -1,29 +1,15 @@
-import requests # install requests because I want to make a GET request to the API
-import os
+
 import openai
+
 # Set the OpenAI API key using the openai.api_key variable
-openai.api_key = "foobar"
+openai.api_key = api_key = "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"
 
 gpt35turbo = openai.ChatCompletion.create(
   model="gpt-3.5-turbo-0301",
-  messages=[{"role": "user", "content": "what is the inner life of a labrador like?"}]
-)
-#from flask import Flask
-
-#app = Flask(__name__)
-
-# Open the text file containing the API key and read its contents
-with open('../ai_models/openai/key.txt', 'r') as f:
-    openai_api_key = f.read().strip()
-
-# Set the OpenAI API key using the openai.api_key variable
-openai.api_key = openai_api_key
-
-gpt35turbo = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo-0301",
-  messages=[{"role": "user", "content": "what is the inner life of a labrador like?"}]
+  messages=[{"role": "user", "content": "what is a brand message map? can you think about this step by step and write me a high-level breakdown in the style of hemingway and following the recommendations of strunk and white?"}]
 )
 
 gptResponse = gpt35turbo["choices"][0]["message"]["content"]
 
 print( gptResponse + "\n\n")
+
