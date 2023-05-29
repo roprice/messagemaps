784cecfacd83db6d71e8001b5a70dd4c9b1f26b1
rowan <rowan@rowanprice.com>
2023-05-29 18:37:01 +0200

    revisions to flask serverloading

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,17 +8,18 @@ def hello():
 
 @app.route('/extract')
 def extract():
-    return 'Brand name extracted!'
+    return 'Brand name extracted ok!'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 # before running the server, turn on virtual environment:
 # source env/bin/activate, if no (env)
 #
 # then for flask (development mode), run:
 # python3 app.py
+# (include debug=-True for autoreloading and debugging)
 #
 # then for production mode, run:
 # gunicorn --bind=0.0.0.0:8000 --workers=5 app:app
+# OR gunicorn --bind=0.0.0.0:8000 --workers=3 --reload app:app (for auto reloading)
